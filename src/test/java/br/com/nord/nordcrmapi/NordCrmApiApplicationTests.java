40066f0c396813dd2474d85530d9a2c0d5d45276
HadryanSilva <hadryan2001@gmail.com>
2024-02-05 02:19:26 -0300

    [CONFIG #1] - Ajustes no arquivo do github actions

--- src/test/java/br/com/nord/nordcrmapi/NordCrmApiApplicationTests.java
+++ src/test/java/br/com/nord/nordcrmapi/NordCrmApiApplicationTests.java
@@ -6,7 +6,6 @@ import org.springframework.context.annotation.Profile;
 import org.springframework.test.context.ActiveProfiles;
 
 @SpringBootTest
-@ActiveProfiles("test")
 class NordCrmApiApplicationTests {
 
 	@Test
