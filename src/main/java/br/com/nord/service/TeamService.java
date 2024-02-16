a5bef8bc945d54bb5c2d428ae2be92f0ff743adb
HadryanSilva <hadryan2001@gmail.com>
2024-02-16 03:03:57 -0300

    [FEATURE #9] - Implementação de Tabela adicional para o relacionamento Team e User

--- src/main/java/br/com/nord/service/TeamService.java
+++ src/main/java/br/com/nord/service/TeamService.java
@@ -3,8 +3,6 @@ package br.com.nord.service;
 import br.com.nord.exception.NotFoundException;
 import br.com.nord.model.Team;
 import br.com.nord.repository.TeamRepository;
-import br.com.nord.repository.TeamUserRepository;
-import br.com.nord.repository.UserRepository;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.log4j.Log4j2;
 import org.springframework.stereotype.Service;
@@ -17,8 +15,6 @@ import java.util.List;
 public class TeamService {
 
     private final TeamRepository teamRepository;
-    private final UserRepository userRepository;
-    private final TeamUserRepository teamUserRepository;
 
     public List<Team> findAll() {
         return teamRepository.findAll();
