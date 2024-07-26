95dc57e8b2add2847de88cc084921aa53ba26415
Hadryan Silva <hadryan2001@gmail.com>
2024-07-25 22:27:26 -0300

    CORS Removed

--- src/main/java/br/com/nord/api/config/SecurityConfig.java
+++ src/main/java/br/com/nord/api/config/SecurityConfig.java
@@ -37,7 +37,7 @@ public class SecurityConfig {
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
         http
             .csrf(AbstractHttpConfigurer::disable)
-            .cors(CorsConfigurer::disable)
+            .cors(AbstractHttpConfigurer::disable)
             .authorizeHttpRequests(auth ->
                 auth.requestMatchers("/auth/**",
                                 "/swagger-ui/**",
