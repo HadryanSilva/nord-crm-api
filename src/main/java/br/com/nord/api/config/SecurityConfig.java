7e5b88eea0452c0873272dffdb69ddfb1cd87455
Hadryan Silva <hadryan2001@gmail.com>
2024-07-25 22:18:25 -0300

    CORS Removed

--- src/main/java/br/com/nord/api/config/SecurityConfig.java
+++ src/main/java/br/com/nord/api/config/SecurityConfig.java
@@ -10,6 +10,7 @@ import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.config.annotation.web.configurers.CorsConfigurer;
 import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
@@ -36,6 +37,7 @@ public class SecurityConfig {
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
         http
             .csrf(AbstractHttpConfigurer::disable)
+            .cors(CorsConfigurer::disable)
             .authorizeHttpRequests(auth ->
                 auth.requestMatchers("/auth/**",
                                 "/swagger-ui/**",
