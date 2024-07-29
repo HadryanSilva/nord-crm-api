768ac8f5ff1a2513a83e07eac1eff13e4acbf863
Hadryan Silva <hadryan2001@gmail.com>
2024-07-29 19:07:28 -0300

    CORS Error FIX

--- src/main/java/br/com/nord/api/config/SecurityConfig.java
+++ src/main/java/br/com/nord/api/config/SecurityConfig.java
@@ -39,6 +39,7 @@ public class SecurityConfig {
     CorsConfigurationSource corsConfigurationSource() {
         CorsConfiguration configuration = new CorsConfiguration();
         configuration.setAllowedOrigins(List.of("http://localhost:5173"));
+        configuration.addAllowedHeader("*");
         configuration.setAllowedMethods(List.of("GET", "POST", "PUT", "DELETE", "OPTIONS"));
         UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
         source.registerCorsConfiguration("/**", configuration);
